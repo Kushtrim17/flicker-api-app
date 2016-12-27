16bd5d710dfdc4a88dfd761bd01254876a7071a6
Kushtrim Abdiu <kushtrimabdiu@Kushtrims-MBP-2.lan>
2016-12-27 15:23:28 +0100

    Initiating the project folder structure and basic layout

---
+++ javascript/app.js
@@ -0,0 +1,18 @@
+/**
+ * @author Kushtrim Abdiu
+ */
+
+/**
+ * appLoaded - triggered when the application loads
+ */
+appLoaded = () => {
+    
+}
+
+/**
+ * searchForPictures - triggered when the button Search is clicked
+ */
+searchForPictures = () => {
+    let userInput = document.getElementById('searchInput').value;
+    alert('this is the input : ' + userInput);
+}
